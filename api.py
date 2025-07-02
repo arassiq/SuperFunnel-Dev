ac34fe2b85d36b8b43be163b25d67c5a7fe12449
Aaron Rassiq <amrassiq@dons.usfca.edu>
2025-07-01 21:50:20 -0700

    tried to change to ipv4 transaction pooler instead of ipv6 (not supported outbound by heroku

--- api.py
+++ api.py
@@ -85,7 +85,7 @@ def runTaskGen():
 
     # Connect to the database
     try:
-        connection = psycopg2.connect(os.getenv("DBURISTRING"))
+        connection = psycopg2.connect(os.getenv("DBURISTRING"), sslmode = 'required')
         print("Connection successful!")
         
         cursor = connection.cursor()
